8b16ddd435d4d08a0fc1b902665aae2329c12e5d
Mason Hall <71787606+hallmasonc@users.noreply.github.com>
2025-01-20 01:34:36 -0500

    Added xorg config and removed unnecessary "base" configuration from stowit.sh.

--- stowit.sh
+++ stowit.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-# what directories should be installable by all users including the root user
-base=(
+# folders that should, or only need to be installed for a local user
+useronly=(
     alacritty
     autorandr
     #bash
-    i3
-)
-
-# folders that should, or only need to be installed for a local user
-useronly=(
     git
+    i3
+    xorg
 )
 
-cd ".dotfiles/"
+# change directory
+cd ~/.dotfiles/
 
 # run the stow command for the passed in directory ($2) in location $1
 stowit() {
@@ -25,17 +23,12 @@ stowit() {
     stow -v -R -t ${usr} ${app}
 }
 
-echo ""
 echo "###    Stowing apps for user: $(whoami)    ###"
-
-# install apps available to local users and root
-for app in ${base[@]}; do
-    stowit "${HOME}" $app
-done
+echo ""
 
 # install only user space folders
 for app in ${useronly[@]}; do
-    if [[! "$(whoami)" = *"root"*]]; then
+    if [[ "$(whoami)" != "root" ]]; then
         stowit "${HOME}" $app
     fi
 done
